5e0a4632460e68c61bf7f07e8ad598089928d571
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-08-27 20:57:27 +0600

    add the assignment and also the solution

---
+++ nextLine.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+    cout<<"hello brother\n";
+    cout<<1+3<<"\n";
+    return 0;
+
+}
---
+++ precision.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main (){
+    float pi=3.14567891011;
+    double pi2=3.14567891011;
+    cout<<setprecision(12)<<pi<<endl;
+    cout<<setprecision(12)<<pi2<<endl;
+    //single line comment 
+    // multi 
+    // line 
+    // comment 
+    return 0;
+
+}
