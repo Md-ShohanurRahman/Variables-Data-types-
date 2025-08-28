358becb8bf75d1ccd638fa032d48129a9a2f2adb
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-08-28 08:25:40 +0600

    explore  the precision and itds header file iomanip

--- precision.cpp
+++ precision.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 int main (){
     float pi=3.14567891011;
-    double pi2=3.14567891011;
-    cout<<setprecision(12)<<pi<<endl;
+    double pi2=3.14567891011121314;
+    double distance = 9.0123456789101112131415;
+    double distance2 = 9.0123456;
+    cout<<setprecision(12)<<pi<<endl; //  precision sets the total number of significant digits to display, not just decimal places.
     cout<<setprecision(12)<<pi2<<endl;
+    cout<<distance<<endl;
+
     //single line comment 
     // multi 
     // line 
