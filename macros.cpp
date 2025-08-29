57d1b1e8ff98093f984519738e9bfe89947d11e5
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-08-29 18:11:32 +0600

    add another define

--- macros.cpp
+++ macros.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#define PI 3.14
+#define PI 3.14 
+#define ll long long
 using namespace std;
 int main (){
-
+    ll a = 9090;
+    cout<< a<<endl;
     cout<< PI<< endl;
     return 0;
 
